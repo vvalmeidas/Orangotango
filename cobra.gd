c35601407cb71e12282223a7d32e836b36d711c3
vvalmeida <vvalmeida96@gmail.com>
2018-01-26 11:23:59 -0300

    Add som ao tocar cobra; ainda vou melhorar

--- cobra.gd
+++ cobra.gd
@@ -13,6 +13,7 @@ func _ready():
 func _fixed_process(delta):
 	move(Vector2(0,0))
 	if(is_colliding() and get_node("Sprite")!=null and get_node("CollisionPolygon2D")!= null):
+		get_node("/root/Node2D/SamplePlayer2D").play("Hit_Hurt10")
 		moko.set("pontos", moko.get("pontos") - 5)
 		get_node("Sprite").queue_free()
 		get_node("CollisionPolygon2D").queue_free()
